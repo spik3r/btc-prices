2e1b4e40e7542975b7325ed3d979dd1dbc2e1a1f
Kai Tait <kai.tait@rewe-digital.com>
2018-10-12 11:27:35 +0200

    startng to play with express

---
+++ pages/turgut.js
@@ -0,0 +1,15 @@
+import Fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+import Prices from "../components/Prices";
+
+const Turgut = () => (
+
+    <Layout>
+        <div>
+            <h1>Hallo Turgut</h1>
+        </div>
+    </Layout>
+);
+
+
+export default Turgut;
