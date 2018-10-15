71910eb732c4a1ea227794f631fecdaa2599577d
Turgut Genc <turgut.genc@rewe-digital.com>
2018-10-15 12:06:41 +0200

    return data

--- api/Person.js
+++ api/Person.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 var data = [{id:1, name: "one"},{id: 2, name: "two"}];
-module.exports = function(){
-    return data;
-};
 
-router.get('/', function(req, res){
-    res.send('GET route on things.');
+router.get('/data', function(req, res){
+    res.json(data);
+});
+
+router.get('/hello', function(req, res){
+    res.send("Hello World!");
 });
 
 module.exports = router;
